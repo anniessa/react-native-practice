d21c1f79e003565d5493581668006e06e7b639ba
Anniessa Antar <anniessa.antar@gmail.com>
2023-05-12 14:25:02 +0100

    styling solution

--- App.js
+++ App.js
@@ -4,21 +4,45 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 const App = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={[styles.container, styles.pink]}>
-        <Text>Hello, world!</Text>
+      <View style={[styles.container, styles.cyan]}>
+        <Text style={styles.text}>Cyan</Text>
+      </View>
+      <View style={[styles.container, styles.blue]}>
+        <Text style={styles.text}>Blue</Text>
+      </View>
+      <View style={[styles.container, styles.magenta]}>
+        <Text style={styles.text}>Magenta</Text>
+      </View>
+      <View style={[styles.container, styles.orange]}>
+        <Text style={styles.text}>Orange</Text>
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  pink: {
-    backgroundColor: 'pink',
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
   },
   container: {
     alignItems: 'center',
     justifyContent: 'center',
-    flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    margin: 10,
   },
   safeArea: {
     flex: 2,
