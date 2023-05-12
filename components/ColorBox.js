df222333bb44dc0f8b6e31fbbcaab06290d3afb2
Anniessa Antar <anniessa.antar@gmail.com>
2023-05-12 15:46:43 +0100

    practiced using FlatList component to display array

--- components/ColorBox.js
+++ components/ColorBox.js
@@ -2,33 +2,42 @@ import React from 'react';
 
 import { View, Text, StyleSheet } from 'react-native';
 
-const ColorBox = ({ colorName, hexCode }) => {
-  const boxColor = {
-    backgroundColor: hexCode,
+const ColorBox = (props) => {
+  const colorStyle = {
+    backgroundColor: props.hexCode,
   };
+
+  const textStyle = {
+    color:
+      // algorithm that takes the hex code and compares it to white
+      // eslint-disable-next-line prettier/prettier
+    //  if the RGB value are in the end 10% of the white spectrum,
+      // you can adjust for that color scheme
+      parseInt(props.hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+        ? 'black'
+        : 'white',
+  };
+
   return (
-    <View style={[styles.container, boxColor]}>
-      <Text style={styles.text}>
-        {colorName}: {hexCode}
+    <View style={[styles.box, colorStyle]}>
+      <Text style={[styles.text, textStyle]}>
+        {props.colorName} {props.hexCode}
       </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  box: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
   text: {
     fontWeight: 'bold',
     color: 'white',
-  },
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 10,
-    paddingHorizontal: 30,
-    margin: 10,
-  },
-  safeArea: {
-    flex: 2,
   },
 });
 
